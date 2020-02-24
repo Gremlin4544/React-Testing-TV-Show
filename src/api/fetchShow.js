14c799d9bd07ae6c48518bd62aa1021bb1763447
Gremlin4544 <alve@mckethen.com>
2020-02-24 17:07:01 -0500

    created api folder and new files, moved fetchShow to new file

---
+++ src/api/fetchShow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+
+const fetchshow = () => {
+    return (
+        axios
+        .get(
+            "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+          )
+        .then(res => {
+            return res;
+            })
+        .catch(error => {
+            return error;
+        })     
+    )
+}
+
+export default fetchshow;
